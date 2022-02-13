abcc04c552ff4fd341ce4fc97c2a7a2252396bb2
Monet Man <88108211+jpcisneros411@users.noreply.github.com>
2022-02-12 18:29:08 -0600

    still working on routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -58,6 +58,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try{
+    Tag = Tag.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    if (!Tag){
+      res.status(404).json({ message: 'No tag found with this id!'});
+      return;
+    }
+
+      res.status(200).json(Tag);
+    } catch (err) {
+      res.status(500).json(err);
+    }     
+  
 });
 
 module.exports = router;
